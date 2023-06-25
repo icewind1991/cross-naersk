7b44b290ac2fffc8e43f0f729bf37d6a3fd6c2c7
Robin Appelman <robin@icewind.nl>
2023-06-25 22:05:54 +0200

    more native-dep wip

--- default.nix
+++ default.nix
@@ -7,73 +7,82 @@
   naersk,
   stdenv,
   fetchurl,
+  rustVersion ? "stable",
 }: target: let
+  inherit (lib.strings) replaceStrings toUpper;
+  inherit (builtins) removeAttrs foldl';
+  inherit (lib.attrsets) recursiveUpdate;
   mingw_w64_cc = pkgsCross.mingwW64.stdenv.cc;
   windows = pkgsCross.mingwW64.windows;
 
-  crossArgs = options:
-    {
-      "armv7-unknown-linux-musleabihf" = let
-        cc = pkgsCross.armv7l-hf-multiplatform.stdenv.cc;
-      in {
-        CARGO_TARGET_ARMV7_UNKNOWN_LINUX_MUSLEABIHF_RUSTFLAGS = "-C target-feature=+crt-static";
-        CARGO_TARGET_ARMV7_UNKNOWN_LINUX_MUSLEABIHF_LINKER = "${cc}/bin/${cc.targetPrefix}cc";
-      };
-      "aarch64-unknown-linux-musl" = let
-        cc = pkgsCross.aarch64-multiplatform-musl.stdenv.cc;
-      in {
-        buildInputs = [cc];
-        CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_RUSTFLAGS = "-C target-feature=+crt-static";
-        CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER = "${cc.targetPrefix}cc";
-        AR_aarch64_unknown_linux_musl = "${cc.targetPrefix}ar";
-        CC_aarch64_unknown_linux_musl = "${cc.targetPrefix}cc";
-        CCX_aarch64_unknown_linux_musl = "${cc.targetPrefix}ccx";
-      };
-      "i686-unknown-linux-musl" = let
-        cc = pkgsCross.musl32.stdenv.cc;
-      in {
-        CARGO_TARGET_I686_UNKNOWN_LINUX_MUSL_RUSTFLAGS = "-C target-feature=+crt-static";
-        CARGO_TARGET_I686_UNKNOWN_LINUX_MUSL_LINKER = "${cc}/bin/${cc.targetPrefix}cc";
-      };
-      "x86_64-pc-windows-gnu" = let
-        cc = pkgsCross.mingwW64.stdenv.cc;
-      in {
-        strictDeps = true;
-        # only add pthreads when building the final package, not when building the dependencies
-        # otherwise it interferes with building build scripts
-        overrideMain = args: args // {buildInputs = [windows.pthreads];};
+  freebsdLib = stdenv.mkDerivation rec {
+    pname = "freebsd-base-libs";
+    version = "13.2-amd64";
+    src = fetchurl {
+      url = "https://download.freebsd.org/ftp/releases/amd64/13.2-RELEASE/base.txz";
+      sha256 = "sha256-OpJQ96/XMLvidGkYWXVpSLPFepm82jDWXUauMAJZBvA=";
+    };
+    sourceRoot = ".";
+    doBuild = false;
+    dontFixup = true;
+    installPhase = ''
+      mkdir -p $out/lib/
+      cp lib/*.so usr/lib/*.so $out/lib/
+    '';
+  };
 
-        CARGO_TARGET_X86_64_PC_WINDOWS_GNU_LINKER = "${cc}/bin/${cc.targetPrefix}cc";
-      };
-      "x86_64-unknown-freebsd" = let
-        cc = pkgsCross.x86_64-freebsd.stdenv.cc;
-        bsdLib = stdenv.mkDerivation rec {
-          pname = "freebsd-base-libs";
-          version = "13.2-amd64";
-          src = fetchurl {
-            url = "https://download.freebsd.org/ftp/releases/amd64/13.2-RELEASE/base.txz";
-            sha256 = "sha256-OpJQ96/XMLvidGkYWXVpSLPFepm82jDWXUauMAJZBvA=";
-          };
-          sourceRoot = ".";
-          doBuild = false;
-          dontFixup = true;
-          installPhase = ''
-            mkdir -p $out/lib/
-            cp lib/*.so usr/lib/*.so $out/lib/
-          '';
-        };
-      in {
-        nativeBuildInputs = [cc bsdLib];
-        CARGO_TARGET_X86_64_UNKNOWN_FREEBSD_RUSTFLAGS = "-C target-feature=+crt-static";
-        CARGO_TARGET_X86_64_UNKNOWN_FREEBSD_LINKER = "${cc.targetPrefix}cc";
-      };
-    }
-    // (options.crossArgs or {});
+  recursiveMerge = callPackage ./merge.nix {};
+
+  buildCrossArgs = target: {
+    rustFlags ? "-C target-feature=+crt-static",
+    cc,
+    ...
+  } @ args: let
+    targetUnderscore = replaceStrings ["-"] ["_"] target;
+    targetUpperCase = toUpper targetUnderscore;
+    rest = removeAttrs args ["rustFlags" "cc"];
+  in (recursiveMerge [{
+      buildInputs = [cc];
+      "CARGO_TARGET_${targetUpperCase}_RUSTFLAGS" = rustFlags;
+      "CARGO_TARGET_${targetUpperCase}_LINKER" = "${cc.targetPrefix}cc";
+      "AR_${targetUnderscore}" = "${cc.targetPrefix}ar";
+      "CC_${targetUnderscore}" = "${cc.targetPrefix}cc";
+      "CCX_${targetUnderscore}" = "${cc.targetPrefix}ccx";
+    } rest]);
+
+  defaultCrossArgs = {
+    "armv7-unknown-linux-musleabihf" = buildCrossArgs "armv7-unknown-linux-musleabihf" {
+      cc = pkgsCross.armv7l-hf-multiplatform.stdenv.cc;
+    };
+    "armv7-unknown-linux-gnueabihf" = buildCrossArgs "armv7-unknown-linux-gnueabihf" {
+      cc = pkgsCross.armv7l-hf-multiplatform.stdenv.cc;
+    };
+    "aarch64-unknown-linux-musl" = buildCrossArgs "aarch64-unknown-linux-musl" {
+      cc = pkgsCross.aarch64-multiplatform-musl.stdenv.cc;
+    };
+    "i686-unknown-linux-musl" = buildCrossArgs "i686-unknown-linux-musl" {
+      cc = pkgsCross.musl32.stdenv.cc;
+    };
+    "x86_64-pc-windows-gnu" = buildCrossArgs "x86_64-pc-windows-gnu" {
+      cc = pkgsCross.mingwW64.stdenv.cc;
+      strictDeps = true;
+      overrideMain = args: args // {buildInputs = [windows.pthreads];};
+    };
+    "x86_64-unknown-freebsd" = buildCrossArgs "x86_64-unknown-freebsd" {
+      cc = pkgsCross.x86_64-freebsd.stdenv.cc;
+      buildInputs = [freebsdLib];
+    };
+    "x86_64-unknown-linux-musl" = (buildCrossArgs "x86_64-unknown-linux-musl" {
+      cc = pkgsCross.musl64.stdenv.cc;
+    });
+  };
+
+  crossArgs = options: recursiveMerge [defaultCrossArgs (options.crossArgs or {})];
 
   naersk' = callPackage naersk {};
   hostTarget = hostPlatform.config;
   naerskForTarget = let
-    toolchain = rust-bin.stable.latest.default.override {targets = [target];};
+    toolchain = rust-bin.${rustVersion}.latest.default.override {targets = [target];};
   in
     callPackage naersk {
       cargo = toolchain;
@@ -89,6 +98,8 @@
     }
     // (builtins.removeAttrs options ["crossArgs"])
     // (crossArgsForTarget options);
-in {
+in rec {
   buildPackage = options: naerskForTarget.buildPackage (args options);
+  defaultCrossArgsFor = target: defaultCrossArgs.${target} or {};
+  defaultCrossArgsForTargets = targets: recursiveMerge (map defaultCrossArgsFor targets);
 }
