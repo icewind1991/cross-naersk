7c0dee0c629c5e618bfe295e669ef9c2481722b6
Robin Appelman <robin@icewind.nl>
2026-07-20 23:29:40 +0200

    don't set CC to not confuse some build scripts

--- default.nix
+++ default.nix
@@ -9,10 +9,16 @@
   perl,
   mkShell,
   freebsdCross,
-  toolchain ? rust-bin.stable.latest.default
-} @ inputs: let
-  inherit (lib.strings) replaceStrings toUpper concatStrings hasInfix;
-  inherit (builtins) removeAttrs foldl' map;
+  toolchain ? rust-bin.stable.latest.default,
+}@inputs:
+let
+  inherit (lib.strings)
+    replaceStrings
+    toUpper
+    concatStrings
+    hasInfix
+    ;
+  inherit (builtins) removeAttrs map;
 
   freebsdSysrootX86 = callPackage ./freebsd-sysroot.nix {
     arch = "amd64";
@@ -20,38 +26,51 @@
     version = "15.0";
   };
 
-  recursiveMerge = callPackage ./merge.nix {};
+  recursiveMerge = callPackage ./merge.nix { };
   isMusl = hasInfix "-musl";
 
-  buildCrossArgs = target: {
-    targetDeps ? [],
-    rustFlags ? (if isMusl target then "-C target-feature=+crt-static" else ""),
-    cFlags ? "",
-    targetStdenv,
-    ...
-  } @ args: let
-    isHostTarget = targetStdenv.targetPlatform.config == stdenv.targetPlatform.config;
-    # don't use the pkgsCross cc if the target is the host platform
-    targetCc = if isHostTarget then stdenv.cc else targetStdenv.cc;
-    targetUnderscore = replaceStrings ["-"] ["_"] target;
-    targetUpperCase = toUpper targetUnderscore;
-    rest = removeAttrs args ["rustFlags" "cc" "cFlags" "targetDeps"];
-    # by adding the dependency in the (target specific) linker args instead of buildInputs
-    # we can prevent it trying to link to it for host build dependencies
-    rustFlagsWithDeps = rustFlags + concatStrings (map (targetDep: " -Clink-arg=-L${targetDep}/lib") targetDeps);
-  in (recursiveMerge [
+  buildCrossArgs =
+    target:
     {
-      nativeBuildInputs = [targetCc stdenv.cc];
-      "CARGO_TARGET_${targetUpperCase}_RUSTFLAGS" = rustFlagsWithDeps;
-      "CARGO_TARGET_${targetUpperCase}_LINKER" = "${targetCc.targetPrefix}cc";
-      "AR_${targetUnderscore}" = "${targetCc.targetPrefix}ar";
-      "CC_${targetUnderscore}" = "${targetCc.targetPrefix}cc";
-      "CCX_${targetUnderscore}" = "${targetCc.targetPrefix}ccx";
-      "HOST_CC" = "${stdenv.cc.targetPrefix}cc";
-      "CFLAGS_${targetUnderscore}" = cFlags;
-    }
-    rest
-  ]);
+      targetDeps ? [ ],
+      rustFlags ? (if isMusl target then "-C target-feature=+crt-static" else ""),
+      cFlags ? "",
+      targetStdenv,
+      ...
+    }@args:
+    let
+      isHostTarget = targetStdenv.targetPlatform.config == stdenv.targetPlatform.config;
+      # don't use the pkgsCross cc if the target is the host platform
+      targetCc = if isHostTarget then stdenv.cc else targetStdenv.cc;
+      hostCc = stdenv.cc;
+      targetUnderscore = replaceStrings [ "-" ] [ "_" ] target;
+      targetUpperCase = toUpper targetUnderscore;
+      rest = removeAttrs args [
+        "rustFlags"
+        "cc"
+        "cFlags"
+        "targetDeps"
+      ];
+      # by adding the dependency in the (target specific) linker args instead of buildInputs
+      # we can prevent it trying to link to it for host build dependencies
+      rustFlagsWithDeps =
+        rustFlags + concatStrings (map (targetDep: " -Clink-arg=-L${targetDep}/lib") targetDeps);
+    in
+    (recursiveMerge [
+      {
+        nativeBuildInputs = [
+          # stdenv.cc
+        ];
+        "CARGO_TARGET_${targetUpperCase}_RUSTFLAGS" = rustFlagsWithDeps;
+        "CARGO_TARGET_${targetUpperCase}_LINKER" = "${targetCc}/bin/${targetCc.targetPrefix}cc";
+        "AR_${targetUnderscore}" = "${targetCc}/bin/${targetCc.targetPrefix}ar";
+        "CC_${targetUnderscore}" = "${targetCc}/bin/${targetCc.targetPrefix}cc";
+        "CCX_${targetUnderscore}" = "${targetCc}/bin/${targetCc.targetPrefix}ccx";
+        "HOST_CC" = "${hostCc}/bin/${stdenv.cc.targetPrefix}cc";
+        "CFLAGS_${targetUnderscore}" = cFlags;
+      }
+      rest
+    ]);
 
   defaultCrossArgs = {
     "armv7-unknown-linux-musleabihf" = buildCrossArgs "armv7-unknown-linux-musleabihf" {
@@ -77,13 +96,13 @@
       targetStdenv = pkgsCross.mingwW64.stdenv;
       strictDeps = true;
       # rink wants perl for windows targets
-      buildInputs = [perl];
-      targetDeps = [pkgsCross.mingwW64.windows.pthreads];
+      buildInputs = [ perl ];
+      targetDeps = [ pkgsCross.mingwW64.windows.pthreads ];
       rustFlags = "-C target-feature=+crt-static";
     };
     "x86_64-unknown-freebsd" = buildCrossArgs "x86_64-unknown-freebsd" {
       targetStdenv = pkgsCross.x86_64-freebsd.stdenv;
-      targetDeps = [freebsdSysrootX86];
+      targetDeps = [ freebsdSysrootX86 ];
       dontPatchELF = true;
       postInstall = ''
         patchelf --set-interpreter /libexec/ld-elf.so.1 $out/bin/*
@@ -103,41 +122,53 @@
     cargo = toolchain;
     rustc = toolchain;
   };
-  crossArgs = options: recursiveMerge [defaultCrossArgs (options.crossArgs or {})];
-in rec {
-  buildPackage = target: let
-    targetToolchain = toolchain.override {targets = [target];};
-    naerskForTarget = callPackage naersk {
-      cargo = targetToolchain;
-      rustc = targetToolchain;
-    };
-    crossArgsForTarget = options:
-      if hostTarget != target
-      then (crossArgs options).${target} or {}
-      else {};
-    args = options:
-      {
-        CARGO_BUILD_TARGET = target;
-      }
-      // (builtins.removeAttrs options ["crossArgs"])
-      // (crossArgsForTarget options);
-  in
+  crossArgs =
+    options:
+    recursiveMerge [
+      defaultCrossArgs
+      (options.crossArgs or { })
+    ];
+in
+rec {
+  buildPackage =
+    target:
+    let
+      targetToolchain = toolchain.override { targets = [ target ]; };
+      naerskForTarget = callPackage naersk {
+        cargo = targetToolchain;
+        rustc = targetToolchain;
+      };
+      crossArgsForTarget =
+        options: if hostTarget != target then (crossArgs options).${target} or { } else { };
+      args =
+        options:
+        {
+          CARGO_BUILD_TARGET = target;
+        }
+        // (builtins.removeAttrs options [ "crossArgs" ])
+        // (crossArgsForTarget options);
+    in
     options: naerskForTarget.buildPackage (args options);
-  defaultCrossArgsForTargets = targets: recursiveMerge (map (target: defaultCrossArgs.${target} or {}) targets);
+  defaultCrossArgsForTargets =
+    targets: recursiveMerge (map (target: defaultCrossArgs.${target} or { }) targets);
   execSufficForTarget = target: if lib.strings.hasInfix "windows" target then ".exe" else "";
   hostNaersk = naersk';
-  mkShell = targets: args: let
-    nonDeps = removeAttrs (defaultCrossArgsForTargets targets) ["nativeBuildInputs"];
-    deps = (defaultCrossArgsForTargets targets).nativeBuildInputs;
-  in
-    inputs.mkShell (nonDeps
+  mkShell =
+    targets: args:
+    let
+      nonDeps = removeAttrs (defaultCrossArgsForTargets targets) [ "nativeBuildInputs" ];
+      deps = (defaultCrossArgsForTargets targets).nativeBuildInputs;
+    in
+    inputs.mkShell (
+      nonDeps
       // args
       // {
         nativeBuildInputs =
           deps
-          ++ (args.nativeBuildInputs or [])
+          ++ (args.nativeBuildInputs or [ ])
           ++ [
-            (toolchain.override {targets = targets ++ [hostTarget];})
+            (toolchain.override { targets = targets ++ [ hostTarget ]; })
           ];
-      });
+      }
+    );
 }
--- flake.nix
+++ flake.nix
@@ -1,17 +1,28 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs/release-25.11";
+    nixpkgs.url = "nixpkgs/release-26.05";
     naersk.url = "github:nix-community/naersk";
     naersk.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    naersk,
-  }: let
-    forAllSystems = nixpkgs.lib.genAttrs ["x86_64-linux" "x86_64-darwin" "i686-linux" "aarch64-linux" "aarch64-darwin"];
-  in {
-    lib = forAllSystems (system: nixpkgs.legacyPackages."${system}".callPackage ./default.nix {inherit naersk;});
-  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      naersk,
+    }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "i686-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+    in
+    {
+      lib = forAllSystems (
+        system: nixpkgs.legacyPackages."${system}".callPackage ./default.nix { inherit naersk; }
+      );
+    };
 }
